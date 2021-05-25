455eb37aeb0cb990db56ee98d5eba2489527d0c1
sajibalam88 <69287413+sajibalam88@users.noreply.github.com>
2021-05-24 21:29:24 -0400

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
         return "Hello World\n";
+    }
+    @RequestMapping("/hi")
+    public String index() {
+        return "Hi World\n";
     }
     
 }
