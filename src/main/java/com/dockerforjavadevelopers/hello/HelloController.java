ec624fc8b7c9d522ffc13b9e60edef449ead9cc3
sajibalam88 <69287413+sajibalam88@users.noreply.github.com>
2021-05-25 00:41:00 -0400

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/hello")
     public String index() {
-        return "Hello All\n";
+        return "Hello All Busy Monday\n";
     }
     @RequestMapping("/hi")
     public String hi() {
